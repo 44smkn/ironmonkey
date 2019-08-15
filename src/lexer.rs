789b05ce22dff5fd95ab79b20a97235b232466ca
Kenji Shimizu <jrywm121@gmail.com>
2019-08-16 00:01:12 +0900

    add testcases

--- src/lexer.rs
+++ src/lexer.rs
@@ -58,10 +58,10 @@ mod tests {
     }
 
     impl ExpectedToken {
-        fn new_token(expected_type: token::TokenType, literal: String) -> Self {
+        fn new_token(expected_type: token::TokenType, literal: &str) -> Self {
             Self {
                 expected_type,
-                literal,
+                literal: literal.to_string(),
             }
         }
     }
@@ -71,7 +71,6 @@ mod tests {
         use super::token::TokenType::*;
         use super::*;
 
-        let nul: char = 0x00_u8.into();
         let input = "let five = 5;
 let ten = 10;
 
@@ -82,15 +81,43 @@ let add = fn(x, y) {
 let result = add(five, ten);
 ";
         let tests = [
-            ExpectedToken::new_token(ASSIGN, "=".to_string()),
-            ExpectedToken::new_token(PLUS, "+".to_string()),
-            ExpectedToken::new_token(LPAREN, "(".to_string()),
-            ExpectedToken::new_token(RPAREN, ")".to_string()),
-            ExpectedToken::new_token(LBRACE, "{".to_string()),
-            ExpectedToken::new_token(RBRACE, "}".to_string()),
-            ExpectedToken::new_token(COMMA, ",".to_string()),
-            ExpectedToken::new_token(SEMICOLON, ";".to_string()),
-            ExpectedToken::new_token(EOF, "".to_string()),
+            ExpectedToken::new_token(LET, "let"),
+            ExpectedToken::new_token(IDENT, "five"),
+            ExpectedToken::new_token(ASSIGN, "="),
+            ExpectedToken::new_token(INT, "5"),
+            ExpectedToken::new_token(SEMICOLON, ";"),
+            ExpectedToken::new_token(LET, "let"),
+            ExpectedToken::new_token(IDENT, "ten"),
+            ExpectedToken::new_token(ASSIGN, "="),
+            ExpectedToken::new_token(INT, "10"),
+            ExpectedToken::new_token(SEMICOLON, ";"),
+            ExpectedToken::new_token(LET, "let"),
+            ExpectedToken::new_token(IDENT, "add"),
+            ExpectedToken::new_token(ASSIGN, "="),
+            ExpectedToken::new_token(FUNCTION, "fn"),
+            ExpectedToken::new_token(LPAREN, "("),
+            ExpectedToken::new_token(IDENT, "x"),
+            ExpectedToken::new_token(COMMA, ","),
+            ExpectedToken::new_token(IDENT, "y"),
+            ExpectedToken::new_token(RPAREN, ")"),
+            ExpectedToken::new_token(LBRACE, "{"),
+            ExpectedToken::new_token(IDENT, "x"),
+            ExpectedToken::new_token(PLUS, "+"),
+            ExpectedToken::new_token(IDENT, "y"),
+            ExpectedToken::new_token(SEMICOLON, ";"),
+            ExpectedToken::new_token(RBRACE, "}"),
+            ExpectedToken::new_token(SEMICOLON, ";"),
+            ExpectedToken::new_token(LET, "let"),
+            ExpectedToken::new_token(IDENT, "result"),
+            ExpectedToken::new_token(ASSIGN, "="),
+            ExpectedToken::new_token(IDENT, "add"),
+            ExpectedToken::new_token(LPAREN, "("),
+            ExpectedToken::new_token(IDENT, "five"),
+            ExpectedToken::new_token(COMMA, ","),
+            ExpectedToken::new_token(IDENT, "ten"),
+            ExpectedToken::new_token(RPAREN, ")"),
+            ExpectedToken::new_token(SEMICOLON, ";"),
+            ExpectedToken::new_token(EOF, ""),
         ];
 
         let mut l = Lexer::new(input);
